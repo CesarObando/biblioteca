3ac72587a315945628334c66de8b83e713d499b0
Jonathan Bonilla Umaña <jonabu94@hotmail.com>
2016-05-12 18:27:06 -0600

    no message

--- proyectoBiblioteca/app/Http/Controllers/SalaAudiovisualesController.php
+++ proyectoBiblioteca/app/Http/Controllers/SalaAudiovisualesController.php
@@ -8,16 +8,50 @@ use gestorBiblioteca\Http\Requests;
 
 class SalaAudiovisualesController extends Controller
 {
+  public function show($id)
+  {
+    $salaAudiovisuales = \gestorBiblioteca\SalaAudiovisuales::find($id);
+    return view ('salaAudiovisuales/prestar',['salaAudiovisuales'=>$salaAudiovisuales]);
+  }
+
+  public function store(Request $request)
+  {
+    \gestorBiblioteca\SalaAudiovisuales::create([
+      'nombreSolicitante' => $request['nombreSolicitante'],
+      'tema' => $request['tema'],
+      'seccion' => $request['seccion'],
+      'totalEstudiantes' => $request['totalEstudiantes'],
+      'fecha' => $request['fecha'],
+      'horaEntrada' => $request['horaEntrada'],
+      'horaSalida' => $request['horaSalida'],
+      'observaciones' => $request['observaciones'],
+    ]);
+    return redirect ('/');
+  }
+
   public function buscarPrestamos()
   {
     return view ('salaAudiovisuales/buscarPrestamos');
   }
   public function listar()
   {
+    $fecha = $request['fecha'];
+    $hora = $request['hora'];
+    if($fecha=='' || $fecha==null)
+    {
+      $fecha='0000.0.0';
+    }
+    if($hora=='' || $hora==null)
+    {
+      $hora='00.00';
+    }
+    $prestamosAudiovisual = DB::select('CALL buscar_prestamos_salaAudiovisuales(?,?,?)',[$request['nombreSolicitante'],$request['tema'],$fecha,$hora]);
+    return view ('audiovisual/listarPrestamos',compact('prestamosAudiovisual'));
     return view ('salaAudiovisuales/listar');
   }
-  public function prestar()
+
+  public function eliminar($id)
   {
-    return view ('salaAudiovisuales/prestar');
+    
   }
 }
--- proyectoBiblioteca/resources/views/salaAudiovisuales/prestar.blade.php
+++ proyectoBiblioteca/resources/views/salaAudiovisuales/prestar.blade.php
@@ -3,33 +3,33 @@
 <div class="container">
             <h1>Uso de Sala de Audiovisuales</h1>
             <br>
-            <form name="usoSalaAudiovisuales" action="listarUsoSalaAudiovisuales.html">
-                <label for="fecha">Fecha:</label>
-                <input type="date" class="form-control" name="fecha" id="fecha" required="">
-                <br>
-                <label for="tema">Tema:</label>
-                <input type="text" class="form-control" name="tema" required="">
-                <br>
-                <label for="seccion">Sección:</label>
-                <input type="text" class="form-control" name="seccion" required="">
-                <br>
-                <label for="totalEstudiantes">Total estudiantes:</label>
-                <input type="number" class="form-control" name="totalEstudiantes" required="">
-                <br>
-                <label for="docente">Docente:</label>
-                <input type="text" class="form-control" name="docente" required="">
-                <br>
-                <label for="horaEntrada">Hora de entrada:</label>
-                <input type="time" class="form-control" name="horaEntrada" required="">
-                <br>
-                <label for="horaSalida">Hora de salida:</label>
-                <input type="time" class="form-control" name="horaSalida" required="">
-                <br>
-                <label for="observaciones">Observaciones:</label>
-                <input type="text" class="form-control" name="observaciones">
-                <hr>
-                <input type="submit" name="usoSalaAudiovisuales" value="Insertar" class="btn-success btn-lg">
-                <br><br>
-            </form>
-        </div>    
+            {!!Form::open(['route'=>'salaAudiovisuales.store','method'=>'post'])!!}
+            <label for="nombreSolicitante">Nombre del solicitante:</label>
+            <input type="text" class="form-control" name="nombreSolicitante" required="">
+            <br>
+            <label for="tema">Tema:</label>
+            <input type="text" class="form-control" name="tema" required="">
+            <br>
+            <label for="seccion">Sección:</label>
+            <input type="text" class="form-control" name="seccion" required="">
+            <br>
+            <label for="totalEstudiantes">Total estudiantes:</label>
+            <input type="number" class="form-control" name="totalEstudiantes" required="">
+            <br>
+            <label for="fecha">Fecha:</label>
+            <input type="date" class="form-control" name="fecha" id="fecha" required="">
+            <br>
+            <label for="horaEntrada">Hora de entrada:</label>
+            <input type="time" class="form-control" name="horaEntrada" required="">
+            <br>
+            <label for="horaSalida">Hora de salida:</label>
+            <input type="time" class="form-control" name="horaSalida" required="">
+            <br>
+            <label for="observaciones">Observaciones:</label>
+            <input type="text" class="form-control" name="observaciones">
+            <hr>
+            <input type="submit" name="usoSalaAudiovisuales" value="Insertar" class="btn-success btn-lg">
+            <br><br>    
+            {!!Form::close()!!}
+        </div>
 @stop
