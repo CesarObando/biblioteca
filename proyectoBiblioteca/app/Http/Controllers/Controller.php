74048e893bd717a56aeb94b84b1d6057caa53a18
allanMoyaM <allanmm29@gmail.com>
2016-06-07 10:21:06 -0600

    login

--- proyectoBiblioteca/app/Http/Controllers/Controller.php
+++ proyectoBiblioteca/app/Http/Controllers/Controller.php
@@ -11,10 +11,18 @@ use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 class Controller extends BaseController
 {
     use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+    public function _construct(){
+        $this->middleware('auth');
+    }
+
     public function index()
     {
       return view('index');
     }
 
+    public function login()
+    {
+      return view('login.login');
+    }
 
 }
